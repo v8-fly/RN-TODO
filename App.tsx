08ce22eb3ed658ab9e93fa1eef550bdef3003aae
v8-fly <98163507+v8-fly@users.noreply.github.com>
2022-08-15 23:20:02 +0530

    Add initial project setup and black theme

--- App.tsx
+++ App.tsx
@@ -1,20 +1,12 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { StatusBar } from 'expo-status-bar'
+import { StyleSheet, Text, View } from 'react-native'
+import AppContainer from './src/components/app-container'
+import Main from './src/screens/main-screen'
 
 export default function App() {
   return (
-    <View style={styles.container}>
-      <Text>Open up App.tsx to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
-  );
+    <AppContainer>
+      <Main></Main>
+    </AppContainer>
+  )
 }
-
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
---
+++ src/screens/main-screen.tsx
@@ -0,0 +1,22 @@
+import * as React from 'react'
+import { Text, Box, Center, VStack, useColorModeValue } from 'native-base'
+import T from '../components/theme-toggle'
+import ThemeToggle from '../components/theme-toggle'
+
+export default function MainScreen() {
+  return (
+    <Center
+      _dark={{ bg: 'blueGray.900' }}
+      _light={{ bg: 'blueGray.50' }}
+      px={4}
+      flex={1}
+    >
+      <VStack space={5} alignItems="center">
+        <Box p={10} bg={useColorModeValue('red.500', 'yellow.500')}>
+          <Text>HELLO</Text>
+        </Box>
+        <ThemeToggle />
+      </VStack>
+    </Center>
+  )
+}
